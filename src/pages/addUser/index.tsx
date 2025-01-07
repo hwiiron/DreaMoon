3225c4d9c4c26ea31eae1512a63c9b5633a886a4
HwichulShin <shinhwiiron@gmail.com>
2025-01-07 21:52:31 +0900

    Firebase 데이터 보내기 테스트

---
+++ src/pages/addUser/index.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import { useState } from "react";
+import db from "@/firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+
+function AddUser() {
+  const [newId, setNewId] = useState("");
+  const [newNickname, setNewNickname] = useState("");
+  const [newAge, setNewAge] = useState<number | string>("");
+
+  const router = useRouter();
+
+  const handleAddUser = async () => {
+    const userDoc = doc(db, "users", newId);
+    await setDoc(userDoc, {
+      nickname: newNickname,
+      age: Number(newAge),
+    });
+
+    setNewId("");
+    setNewNickname("");
+    setNewAge("");
+
+    router.push("/");
+  };
+
+  return (
+    <>
+      <Link href={"/"}>유저 목록</Link>
+
+      <div>
+        <h2>유저 추가</h2>
+        <input
+          type="text"
+          placeholder="이름"
+          value={newId}
+          onChange={(e) => setNewId(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="닉네임"
+          value={newNickname}
+          onChange={(e) => setNewNickname(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="나이"
+          value={newAge}
+          onChange={(e) => setNewAge(e.target.value)}
+        />
+        <button onClick={handleAddUser}>추가</button>
+      </div>
+    </>
+  );
+}
+
+export default AddUser;
