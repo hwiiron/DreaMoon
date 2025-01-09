5996902e05eef5ed103fcd913ca3dae1e785742d
HwichulShin <shinhwiiron@gmail.com>
2025-01-09 20:16:23 +0900

    visual 영상 추가

--- src/components/common/Visual.style.ts
+++ src/components/common/Visual.style.ts
@@ -2,11 +2,19 @@ import styled from "styled-components";
 
 const StyledVisual = styled.div`
   border-radius: 12px;
-  background: url("/images/img_DreaMoon.png") no-repeat center;
-  background-size: 100%;
+  /* background: url("/images/img_DreaMoon.png") no-repeat center;
+  background-size: cover; */
   width: 100%;
   height: 720px;
-  margin-top: 50px;
+  /* height: 500px; */
+  margin-top: 30px;
+  overflow: hidden;
+
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
 
 export { StyledVisual };
--- src/components/common/Visual.tsx
+++ src/components/common/Visual.tsx
@@ -1,7 +1,15 @@
 import { StyledVisual } from "./Visual.style";
 
 function Visual() {
-  return <StyledVisual />;
+  return (
+    <StyledVisual>
+      <video
+        src="../../public/video/video_visual.mp4"
+        muted
+        autoPlay={true}
+      ></video>
+    </StyledVisual>
+  );
 }
 
 export default Visual;
