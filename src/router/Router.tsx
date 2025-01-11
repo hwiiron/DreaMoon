a40f183efae6bd1e386ac9b5d7d1b2f6c84bc879
HwichulShin <shinhwiiron@gmail.com>
2025-01-11 17:17:00 +0900

    배포 실패로 폴더 구조 수정

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../layout/Header";
 import Home from "../pages/home/Home";
 import Signin from "../pages/signin/Signin";
+import MyCalendar from "../pages/mycalendar/MyCalendar";
+import UserList from "../pages/userlist/UserList";
 
 type HeaderProp = {
   isDarkMode: boolean;
@@ -17,6 +19,8 @@ function Router({ isDarkMode, toggleTheme }: HeaderProp) {
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="signin" element={<Signin />} />
+          <Route path="myCalendar" element={<MyCalendar />} />
+          <Route path="userlist" element={<UserList />} />
         </Route>
       </Routes>
     </BrowserRouter>
