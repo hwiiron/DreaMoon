7d9eeb7ff9d21c5ee8e214505f6f176cd733aa29
HwichulShin <shinhwiiron@gmail.com>
2025-01-11 16:22:31 +0900

    오류 메시지 상수화

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,6 +1,4 @@
 export const ERROR_MESSAGE = {
   EMAIL_INVALID_FORMAT: "이메일 형식으로 작성해 주세요.",
   PASSWORD_MIN_LENGTH: "8자 이상 입력해 주세요.",
-  NICKNAME_MIN_LENGTH: "열 자 이하로 작성해 주세요.",
-  PASSWORD_MISMATCH: "비밀번호가 일치하지 않습니다.",
 };
